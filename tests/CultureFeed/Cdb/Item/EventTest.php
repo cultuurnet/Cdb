bde8732cdb01aa30b4c87cfe552016f6b105035a
Kristof Coomans <kristof@coomans.eu>
2013-02-26 13:05:22 +0100

    corrections to test for missing cdbid attribute in event XML element

--- tests/CultureFeed/Cdb/Item/EventTest.php
+++ tests/CultureFeed/Cdb/Item/EventTest.php
@@ -23,7 +23,10 @@ class CultureFeed_Cdb_Item_EventTest extends PHPUnit_Framework_TestCase
 
         $xpath = new DOMXPath($dom);
 
-        $items = $xpath->query('/event/@cdbid');
+        $items = $xpath->query('/events/event');
+        $this->assertEquals(1, $items->length);
+
+        $items = $xpath->query('/events/event/@cdbid');
         $this->assertEquals(0, $items->length);
 
         $uuid = '0FA6D598-F126-4B4F-BCE3-BAF3BD959A35';
@@ -37,7 +40,10 @@ class CultureFeed_Cdb_Item_EventTest extends PHPUnit_Framework_TestCase
 
         $xpath = new DOMXPath($dom);
 
-        $items = $xpath->query('/event/@cdbid');
+        $items = $xpath->query('/events/event');
+        $this->assertEquals(1, $items->length);
+
+        $items = $xpath->query('/events/event/@cdbid');
         $this->assertEquals(1, $items->length);
 
         $this->assertEquals($uuid, $items->item(0)->textContent);
