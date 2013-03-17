555b4176de9de1e20907a7f8139354484e60b6db
Kristof Coomans <kristof@coomans.eu>
2013-03-17 23:53:53 +0100

    Added some tests for CultureFeed_Cdb_Data_Performer.

---
+++ tests/CultureFeed/Cdb/Data/PerformerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+class CultureFeed_Cdb_Data_PerformerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @param $fileName
+     * @return DOMDocument
+     */
+    public function loadSample($fileName) {
+        $sampleDir = __DIR__ . '/samples/PerformerTest/';
+        $filePath = $sampleDir . $fileName;
+
+        $dom = new DOMDocument();
+        $dom->load($filePath);
+
+        return $dom;
+    }
+
+    public function testAppendsLabelElementContainingLabel() {
+        $performer = new CultureFeed_Cdb_Data_Performer();
+        $performer->setLabel('Simon & Garfunkel');
+
+        $dom = new DOMDocument();
+        $performersElement = $dom->createElement('performers');
+        $dom->appendChild($performersElement);
+        $performer->appendToDOM($performersElement);
+
+        $xpath = new DOMXPath($dom);
+        $items = $xpath->query('/performers/performer');
+        $this->assertEquals(1, $items->length);
+
+        $items = $xpath->query('/performers/performer/label');
+        $this->assertEquals(1, $items->length);
+
+        $labelElement = $items->item(0);
+
+        $this->assertEquals('Simon & Garfunkel', $labelElement->textContent);
+    }
+
+    public function testAppendsRoleBeforeLabel() {
+        $sample = $this->loadSample('full_with_label.xml');
+
+        $performer = new CultureFeed_Cdb_Data_Performer();
+        $performer->setLabel('Elvis');
+        $performer->setRole('Zang');
+
+        $dom = new DOMDocument();
+        $performersElement = $dom->createElement('performers');
+        $dom->appendChild($performersElement);
+        $performer->appendToDOM($performersElement);
+
+        $xpath = new DOMXPath($dom);
+        $items = $xpath->query('/performers/performer');
+        $this->assertEquals(1, $items->length);
+
+        $this->assertEqualXMLStructure($sample->documentElement, $items->item(0));
+    }
+
+    public function testDoesNotGenerateEmptyRoleElementWhenNoRoleWasSet() {
+        $sample = $this->loadSample('without_role.xml');
+
+        $performer = new CultureFeed_Cdb_Data_Performer();
+        $performer->setLabel('Elvis');
+
+        $dom = new DOMDocument();
+        $performersElement = $dom->createElement('performers');
+        $dom->appendChild($performersElement);
+        $performer->appendToDOM($performersElement);
+
+        $xpath = new DOMXPath($dom);
+        $items = $xpath->query('/performers/performer');
+        $this->assertEquals(1, $items->length);
+
+        $this->assertEqualXMLStructure($sample->documentElement, $items->item(0));
+    }
+}
