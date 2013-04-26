9f91aa891b77c1b16d44307671f5b773f5552457
Jochen Stals <jochen.stals@wunderkraut.com>
2013-04-26 09:40:59 +0200

    adding new page permissions.

---
+++ lib/CultureFeed/Cdb/Data/PagePermissions.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @class
+ * Representation of an PagePermissions element in the cdb xml.
+ */
+class CultureFeed_Cdb_Data_PagePermissions implements CultureFeed_Cdb_IElement {
+
+  /**
+   * Indicates if the page allows activities.
+   * @var Boolean
+   */
+  public $allowActivities = FALSE;
+  
+  /**
+   * Indicates if the page allows commenting.
+   * @var Boolean
+   */
+  public $allowComments = FALSE;
+  
+  /**
+   * Indicates if the page allows followers.
+   * @var Boolean
+   */
+  public $allowFollowers = FALSE;
+  
+  /**
+   * Indicates if the page allows liking.
+   * @var Boolean
+   */
+  public $allowLikes = FALSE;
+  
+  /**
+   * Indicates if the page allows to become member.
+   * @var Boolean
+   */
+  public $allowMembers = FALSE;
+  
+  /**
+   * Indicates if the page allows to message.
+   * @var Boolean
+   */
+  public $allowMessages = FALSE;
+  
+  /**
+   * Indicates if the page allows to recommend a page.
+   * @var Boolean
+   */
+  public $allowRecommendations = FALSE;
+  
+  /**
+   * @see CultureFeed_Cdb_IElement::appendToDOM()
+   */
+  public function appendToDOM(DOMElement $element) {
+  }
+  
+  /**
+   * @see CultureFeed_Cdb_IElement::parseFromCdbXml(SimpleXMLElement $xmlElement)
+   * @return CultureFeed_Cdb_Item_Page
+   */
+  public static function parseFromCdbXml(SimpleXMLElement $xmlElement) {
+
+    $pagePermissions = new self();
+    
+    $pagePermissions->allowActivities = (string) $xmlElement->allowActivities == "true" ? true : false;
+    $pagePermissions->allowComments = (string) $xmlElement->allowComments == "true" ? true : false;
+    $pagePermissions->allowFollowers = (string) $xmlElement->allowFollowers == "true" ? true : false;
+    $pagePermissions->allowLikes = (string) $xmlElement->allowLikes == "true" ? true : false;
+    $pagePermissions->allowMembers = (string) $xmlElement->allowMembers == "true" ? true : false;
+    $pagePermissions->allowMessages = (string) $xmlElement->allowMessages == "true" ? true : false;
+    $pagePermissions->allowRecommendations = (string) $xmlElement->allowRecommendations == "true" ? true : false;
+    
+    return $pagePermissions;
+  }
+  
+}
