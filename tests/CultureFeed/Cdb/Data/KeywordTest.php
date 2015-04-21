8af2c4c4335c89d5751d8529167bea39cbfc2402
Kristof Coomans <kristof@coomans.eu>
2015-04-21 19:07:36 +0200

    MSS-551: test for valid keyword values

--- tests/CultureFeed/Cdb/Data/KeywordTest.php
+++ tests/CultureFeed/Cdb/Data/KeywordTest.php
@@ -18,4 +18,25 @@ class CultureFeed_Cdb_Data_KeywordTest extends PHPUnit_Framework_TestCase {
     $invisibleKeyword = new CultureFeed_Cdb_Data_Keyword('foo', FALSE);
     $this->assertFalse($invisibleKeyword->isVisible());
   }
+
+  /**
+   * Provider for valid keyword values.
+   *
+   * @return array
+   */
+  public function validKeywordValues() {
+    return array(
+      array('foo'),
+      array('bar'),
+    );
+  }
+
+  /**
+   * @dataProvider validKeywordValues
+   * @param string $validValue
+   */
+  public function testValidValue($validValue) {
+    $keyword = new CultureFeed_Cdb_Data_Keyword($validValue);
+    $this->assertSame($validValue, $keyword->getValue());
+  }
 }
