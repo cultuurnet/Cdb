0ad82c83c02bd52e1e0dcfc1176e6967df4d8715
Kristof Coomans <kristof@coomans.eu>
2015-05-19 13:04:57 +0200

    MSS-151: Fix test

--- tests/CultureFeed/Cdb/DefaultTest.php
+++ tests/CultureFeed/Cdb/DefaultTest.php
@@ -2,20 +2,35 @@
 
 class CultureFeed_Cdb_Default_DefaultTest extends PHPUnit_Framework_TestCase
 {
-    public function testConstructorWithSpecificVersion()
+  public function versionProvider() {
+    return array(
+      array('3.1'),
+      array('3.2'),
+      array('3.3'),
+    );
+  }
+
+  /**
+   * @dataProvider versionProvider
+   * @param string $version
+   */
+    public function testConstructorWithSpecificVersion($version)
     {
-        $cdbXml = new CultureFeed_Cdb_Default('3.2');
+        $cdbXml = new CultureFeed_Cdb_Default($version);
         $this->assertEquals(
-            'http://www.cultuurdatabank.com/XMLSchema/CdbXSD/3.2/FINAL',
+          CultureFeed_Cdb_Xml::namespaceUriForVersion($version),
             $cdbXml->getSchemaUrl()
         );
-        $this->assertEquals('3.2', $cdbXml->getSchemaVersion());
+        $this->assertEquals($version, $cdbXml->getSchemaVersion());
     }
 
     public function testConstructorWithoutSpecificVersion()
     {
       $cdbXml = new CultureFeed_Cdb_Default();
-      $this->assertEquals($cdbXml::CDB_SCHEME_URL, $cdbXml->getSchemaUrl());
-      $this->assertEquals($cdbXml::CDB_SCHEME_VERSION, $cdbXml->getSchemaVersion());
+      $this->assertEquals(
+        CultureFeed_Cdb_Xml::namespaceUriForVersion('3.2'),
+        $cdbXml->getSchemaUrl()
+      );
+      $this->assertEquals('3.2', $cdbXml->getSchemaVersion());
     }
 }
