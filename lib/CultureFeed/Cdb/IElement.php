6838e73bd576775a68248da72830cbf4ccadc17d
Luc Wollants <lucwollants@gmail.com>
2025-08-05 12:10:32 +0200

    Typehint common classes

--- lib/CultureFeed/Cdb/IElement.php
+++ lib/CultureFeed/Cdb/IElement.php
@@ -1,25 +1,7 @@
 <?php
 
-/**
- * Interface for a cdb element.
- */
 interface CultureFeed_Cdb_IElement
 {
-    /**
-     * Appends the current object to the passed DOM tree.
-     *
-     * @param DOMElement $element
-     *   The DOM tree to append to.
-     */
-    public function appendToDOM(DOMElement $element);
-
-    /**
-     * Parse a new object from a given cdbxml element.
-     *
-     * @param SimpleXMLElement $xmlElement
-     *   XML to parse.
-     *
-     * @throws CultureFeed_Cdb_ParseException
-     */
+    public function appendToDOM(DOMElement $element): void;
     public static function parseFromCdbXml(SimpleXMLElement $xmlElement);
 }
--- lib/CultureFeed/Cdb/ParseException.php
+++ lib/CultureFeed/Cdb/ParseException.php
@@ -1,5 +1,5 @@
 <?php
 
-class CultureFeed_Cdb_ParseException extends Exception
+final class CultureFeed_Cdb_ParseException extends Exception
 {
 }
