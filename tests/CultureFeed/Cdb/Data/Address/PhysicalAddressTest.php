a4ccb4b4d126402a79ae642073f5997ec6609592
Luc Wollants <lucwollants@gmail.com>
2025-08-18 15:50:38 +0200

    Handle special characters in `CultureFeed_Cdb_Data_Address_PhysicalAddress`

--- tests/CultureFeed/Cdb/Data/Address/PhysicalAddressTest.php
+++ tests/CultureFeed/Cdb/Data/Address/PhysicalAddressTest.php
@@ -77,4 +77,39 @@ final class CultureFeed_Cdb_Data_Address_PhysicalAddressTest extends TestCase
             ['missing_zipcode.xml'],
         ];
     }
+
+
+    public function testAppendToDom(): void
+    {
+        $physicalAddress = new \CultureFeed_Cdb_Data_Address_PhysicalAddress();
+        $physicalAddress->setStreet('Sint & Gisleinstraat');
+        $physicalAddress->setHouseNumber('61 & 62');
+        $physicalAddress->setZip('1000');
+        $physicalAddress->setCity('Brussel');
+        $physicalAddress->setCountry('BE');
+
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->formatOutput = true;
+        $dom->preserveWhiteSpace = false;
+
+        // The physical address can only be appended to another element,
+        // not to the dom document. So we append it to a temporary root
+        // element first.
+        $rootElement = $dom->createElement('root');
+        $dom->appendChild($rootElement);
+        $physicalAddress->appendToDOM($rootElement);
+
+        // Set <physical> as the root element.
+        $xpath = new DOMXPath($dom);
+        $items = $xpath->query('//physical');
+        $this->assertEquals(1, $items->length);
+        $addressElement = $items->item(0);
+        $dom->removeChild($rootElement);
+        $dom->appendChild($addressElement);
+
+        $expectedCdbXml = file_get_contents(__DIR__ . '/samples/PhysicalAddressTest/street_special_character.xml');
+        $actualCdbXml = $dom->saveXML();
+
+        $this->assertEquals($expectedCdbXml, $actualCdbXml);
+    }
 }
